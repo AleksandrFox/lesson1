835d6d838b814f741a4f9d85082352d4657bc75b
Ams <sasha_ams@mail.ru>
2023-01-15 19:51:30 +0500

    new

--- program.py
+++ program.py
@@ -1,10 +1,9 @@
-game = dict(a = '1', b = '2', c = '3', d = '4', e = '5', f = '6', g = '7', h = '8', j = '9', )
-jrt = True
-while jrt:
-    z = input('   :  ')
-    for item in game.items():
-        if z == item[1]:
-            game[item[0]] = 'X' 
-            jrt = False
-            break
-print(game)
+from datetime import date
+def saturdays_between_two_dates(start, end):
+    return sum(x.weekday() == 5 for x in [date.fromordinal(i) for i in range(min(start.toordinal(), end.toordinal()), max(start.toordinal(), end.toordinal()) + 1)])
+
+
+date1 = date(2020, 7, 26)
+date2 = date(2020, 7, 2)
+
+print(saturdays_between_two_dates(date1, date2))
